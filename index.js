91b378d5074f5dbcfb099634628b1774688c5426
Alx-Lai <b09902135@csie.ntu.edu.tw>
2021-06-28 15:25:26 +0800

    modified port

--- index.js
+++ index.js
@@ -446,6 +446,6 @@ wss.on('connection', function connection(client) {
 
 mongo.connect();
 
-server.listen(8080, () => {
-  console.log('Server listening at http://localhost:8080');
+server.listen(process.env.PORT || 8080, () => {
+  console.log(`Server listening at ${process.env.PORT || 8080}`);
 });
